ad9721d6310049bb56071397f4114b583d88f036
Redshark61 <tim.vannier@gmail.com>
2022-02-11 10:41:04 +0100

    get object or 404

--- health_book/home/views.py
+++ health_book/home/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic.list import ListView
