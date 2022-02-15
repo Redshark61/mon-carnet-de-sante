036e030982b538c8edd6593510e1afef5173edbb
Redshark61 <tim.vannier@gmail.com>
2022-02-15 15:56:45 +0100

    update nav bar when user is medical

--- health_book/home/views/homeView.py
+++ health_book/home/views/homeView.py
@@ -3,6 +3,7 @@ from login_signup.models.appointment import Appointment
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 
 @method_decorator(login_required(login_url='index'), name="get")
@@ -18,5 +19,7 @@ class HomeView(View):
             'user': request.user,
             'appointments': appointment,
         }
+
+        # context['isMedical'] = self.request.user.has_perm('login_signup.can_use_medical_stuff')
 
         return render(request, self.template_name, context)
--- health_book/login_signup/models/prescription.py
+++ health_book/login_signup/models/prescription.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.conf import settings
 from login_signup.models.doctor import Doctor
+from login_signup.models.treatment import Treatment
+
 
 class Prescription(models.Model):
 
     doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    treatment = models.ForeignKey(Treatment, on_delete=models.SET_NULL, null=True)
     prescription = models.TextField()
     end_date = models.DateField(null=True, blank=True)
     is_permanent = models.BooleanField(default=False)
