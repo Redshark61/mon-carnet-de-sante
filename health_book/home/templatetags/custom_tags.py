ca413d50a00d65aa17d838c3c15b61acb5fe7eaa
Redshark61 <tim.vannier@gmail.com>
2022-02-28 11:29:51 +0100

    display notif on nav bar

--- health_book/home/templatetags/custom_tags.py
+++ health_book/home/templatetags/custom_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from login_signup.models.notification import Notification
 
 register = template.Library()
 
@@ -7,4 +8,10 @@ def to_str(value):
     return str(value)
 
 
+def notification_message(value):
+    notification = Notification.objects.filter(for_user=value)
+    return notification
+
+
 register.filter('to_str', to_str)
+register.filter('notification_message', notification_message)
