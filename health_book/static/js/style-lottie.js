6179e5ab69d9ebcab00f503135b16550cd26f945
Redshark61 <tim.vannier@gmail.com>
2022-03-03 14:15:52 +0100

    first basic cstyles

---
+++ health_book/static/js/style-lottie.js
@@ -0,0 +1,5 @@
+let lottie = document.querySelector("lottie-player");
+lottie.shadowRoot.getElementById("animation").style.width = "40vw";
+let animationContainer = lottie.shadowRoot.getElementById("animation-container");
+animationContainer.style.width = "fit-content";
+animationContainer.style.marginInline = "auto";
