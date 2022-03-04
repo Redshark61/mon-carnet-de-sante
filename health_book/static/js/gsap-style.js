fe05fa51951694d32d51b0d0a5cacc825292c7c0
Redshark61 <tim.vannier@gmail.com>
2022-03-04 10:49:13 +0100

    small changes

--- health_book/static/js/gsap-style.js
+++ health_book/static/js/gsap-style.js
@@ -2,13 +2,11 @@ let tl = gsap.timeline({
 	scrollTrigger: {
 		trigger: ".list__item",
 		start: "top center",
-		// end: "top top",
-		// once: true,
 		toggleActions: "play none none play",
 	},
 });
 
-tl.addLabel("start").to(".line-divider", {
+tl.to(".line-divider", {
 	opacity: 1,
 	duration: 2,
 	x: 0,
